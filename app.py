abacc7c54c440c5af470bb5cca334829671520b6
hendisantika <hendisantika@yahoo.co.id>
2019-02-11 21:14:07 +0700

    #2 - Add MySQL Config

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'flask'
+mysql = MySQL(app)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+def home():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM computer")
+    rv = cur.fetchall()
+    cur.close()
+    return render_template('home.html', computers=rv)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
